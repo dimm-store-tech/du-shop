384463acf5ea844019c8f98f2911cd5ba9572fbf
DuberlyIvanMondragonManchay <duberly.mondragon@tecsup.edu.pe>
2023-10-02 22:49:49 -0500

    first commit

--- README.md
+++ README.md
@@ -1,4 +1,2 @@
-# django_plantilla
-Plantilla con un proyecto en django y una app dentro
 ##### pip freeze > requirements.txt => Esto para crea el archivo de requerimientos 
 ##### pip install -r requirements.txt => Esto para instalar los requerimientos
--- app/views.py
+++ app/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
-
+from django.shortcuts import render,get_object_or_404
+from .models import Producto,Categoria
 # Create your views here.
+
+def index(request):
+    product_list = Producto.objects.order_by('nombre')
+    categorie_list = Categoria.objects.order_by('nombre')
+    print(categorie_list)
+    return render(request, 'index.html', {"product_list":product_list,"categorie_list":categorie_list})
+
+
+def producto(request, id):
+    producto = get_object_or_404(Producto, id=id)
+    return render(request, 'producto.html', {'producto': producto})
