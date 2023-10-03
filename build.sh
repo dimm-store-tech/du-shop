3a04a6d99ded21d7d4d142ff2531779b5de29485
DuberlyIvanMondragonManchay <duberly.mondragon@tecsup.edu.pe>
2023-10-03 11:40:17 -0500

    Update file build.sh

--- build.sh
+++ build.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 # exit on error
 set -o errexit
-/opt/render/project/src/.venv/bin/python3.10 -m pip install pywin32
 
 # poetry install
 pip install -r requirements.txt
+
 python manage.py collectstatic --no-input
 python manage.py migrate
